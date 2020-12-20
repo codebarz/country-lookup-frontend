0fa5a19d017ae43271dd134e0f696b7a14b1787c
whitehox <oketegah@gmail.com>
2020-12-21 00:03:36 +0100

    make label props optional for input component

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -2,13 +2,14 @@ import React, { InputHTMLAttributes } from 'react';
 import './styles.css';
 
 interface ComponentProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
   type: string;
   value: string;
   name: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   id: string;
   showLabel?: boolean;
+  aditionalClass?: string;
 }
 
 const Input: React.FC<ComponentProps> = ({
@@ -19,9 +20,11 @@ const Input: React.FC<ComponentProps> = ({
   onChange,
   id,
   showLabel,
+  disabled,
+  aditionalClass,
 }) => {
   return (
-    <div className="input-wrapper">
+    <div className={`input-wrapper ${aditionalClass ? aditionalClass : ''}`}>
       {showLabel ? <label htmlFor={id}>{label}</label> : null}
       <input
         type={type}
@@ -29,6 +32,7 @@ const Input: React.FC<ComponentProps> = ({
         value={value}
         onChange={onChange}
         id={id}
+        disabled={disabled}
       />
     </div>
   );
