bc28d1e0bc5b1af2d919d07ba6ab57d1b98e1c10
whitehox <oketegah@gmail.com>
2020-12-21 13:52:41 +0100

    create currency converter functionality

--- src/action/endpoint.ts
+++ src/action/endpoint.ts
@@ -3,6 +3,7 @@ const API_URI = process.env.REACT_APP_API_URL;
 const endpoints = {
   login: `${API_URI}/api/v1/auth/login`,
   searchCountry: (country: string) => `${API_URI}/api/v1/country/${country}`,
+  convertCurrency: `${API_URI}/api/v1/country/convertcurrency`,
 };
 
 export default endpoints;
