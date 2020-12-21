8297b0d9a4afb759174b20459622274182d8aa3b
whitehox <oketegah@gmail.com>
2020-12-21 09:23:54 +0100

    add protect routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import ProtectedRoute from './components/ProtectedRoutes';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const LoginPage = lazy(() => import('./pages/Login'));
@@ -10,8 +11,10 @@ function App() {
   return (
     <Suspense fallback="Loading...">
       <Router>
+        <ProtectedRoute exact={true} path="/" Component={HomePage} />
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="*" render={() => <div>404 Not found</div>} />
       </Router>
       <Toaster />
     </Suspense>
