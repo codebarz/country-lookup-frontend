4cc858daf33e53ac82d62c30cf7e1998a5f33984
whitehox <oketegah@gmail.com>
2020-12-21 01:40:51 +0100

    add react toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const LoginPage = lazy(() => import('./pages/Login'));
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={LoginPage} />
       </Router>
+      <Toaster />
     </Suspense>
   );
 }
